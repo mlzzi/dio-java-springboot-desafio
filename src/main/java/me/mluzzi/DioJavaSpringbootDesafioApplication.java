f538eb35cde028b220cf675b9411dc5b5097ac7c
mlzzi <muriloluzzi@gmail.com>
2024-09-27 11:15:40 -0300

    Initial commit

---
+++ src/main/java/me/mluzzi/DioJavaSpringbootDesafioApplication.java
@@ -0,0 +1,13 @@
+package me.mluzzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaSpringbootDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJavaSpringbootDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mluzzi/DioJavaSpringbootDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mluzzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaSpringbootDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
